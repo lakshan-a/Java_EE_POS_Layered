f04ca6fda38889a1204b645b246f40f99791d21a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-29 19:13:27 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/impl/CustomerBoImpl.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/impl/CustomerBoImpl.java
@@ -1,4 +1,30 @@
 package lk.ijse.gdse.hello.bo.custom.impl;
 
-public class CustomerBoImpl {
+import lk.ijse.gdse.hello.bo.custom.CustomerBo;
+import lk.ijse.gdse.hello.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CustomerBoImpl implements CustomerBo {
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean saveCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomer(Connection connection, String customerID) throws SQLException, ClassNotFoundException {
+        return false;
+    }
 }
