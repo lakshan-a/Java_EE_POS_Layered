1946f1952a9f1166865ad3a57e59a536dbea0de0
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-29 13:33:26 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/model/CustometDTO.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/model/CustometDTO.java
@@ -1,4 +1,55 @@
 package lk.ijse.gdse.hello.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CustometDTO {
+
+    private String cusId;
+    private String cusName;
+    private String cusAddress;
+    private double cusSalary;
+
+    public CustometDTO(String cusId, String cusName, String cusAddress, double cusSalary) {
+        this.cusId = cusId;
+        this.cusName = cusName;
+        this.cusAddress = cusAddress;
+        this.cusSalary = cusSalary;
+    }
+
+    public String getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(String cusId) {
+        this.cusId = cusId;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public String getCusAddress() {
+        return cusAddress;
+    }
+
+    public void setCusAddress(String cusAddress) {
+        this.cusAddress = cusAddress;
+    }
+
+    public double getCusSalary() {
+        return cusSalary;
+    }
+
+    public void setCusSalary(double cusSalary) {
+        this.cusSalary = cusSalary;
+    }
 }
