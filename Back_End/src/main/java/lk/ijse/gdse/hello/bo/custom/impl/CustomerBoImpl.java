46765a36e2020ebf2dfa9e89b77ed0087a6c4b90
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-29 19:32:02 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/impl/CustomerBoImpl.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/impl/CustomerBoImpl.java
@@ -1,30 +1,39 @@
 package lk.ijse.gdse.hello.bo.custom.impl;
 
 import lk.ijse.gdse.hello.bo.custom.CustomerBo;
+import lk.ijse.gdse.hello.dao.DaoFactory;
+import lk.ijse.gdse.hello.dao.custom.CustomerDao;
 import lk.ijse.gdse.hello.dto.CustomerDTO;
+import lk.ijse.gdse.hello.entity.Customer;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerBoImpl implements CustomerBo {
+    CustomerDao customerDAO = (CustomerDao) DaoFactory.getDaoFactory().getDAO(DaoFactory.DAOTypes.CUSTOMER);
     @Override
     public ArrayList<CustomerDTO> getAllCustomers(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<CustomerDTO> allCustomers= new ArrayList<>();
+        ArrayList<Customer> all = customerDAO.getAll(connection);
+        for (Customer c : all) {
+            allCustomers.add(new CustomerDTO(c.getCusId(),c.getCusName(),c.getCusAddress(),c.getCusSalary()));
+        }
+        return allCustomers;
     }
 
     @Override
     public boolean saveCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
-        return false;
+        return customerDAO.save(connection,new Customer(customerDTO.getCusId(),customerDTO.getCusName(),customerDTO.getCusAddress(),customerDTO.getCusSalary()));
     }
 
     @Override
     public boolean updateCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
-        return false;
+        return customerDAO.update(connection,new Customer(customerDTO.getCusId(),customerDTO.getCusName(),customerDTO.getCusAddress(),customerDTO.getCusSalary()));
     }
 
     @Override
     public boolean deleteCustomer(Connection connection, String customerID) throws SQLException, ClassNotFoundException {
-        return false;
+        return customerDAO.delete(connection,customerID);
     }
 }
