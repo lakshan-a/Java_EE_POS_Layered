3fede39cdf67e03853785afccb25d88ad578b8b7
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-29 19:36:26 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/api/CustomerServlet.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/api/CustomerServlet.java
@@ -1,4 +1,59 @@
 package lk.ijse.gdse.hello.api;
 
-public class CustomerServlet {
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse.hello.bo.BoFactory;
+import lk.ijse.gdse.hello.bo.custom.CustomerBo;
+import lk.ijse.gdse.hello.dto.CustomerDTO;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet(urlPatterns = "/customers")
+public class CustomerServlet extends HttpServlet {
+
+    CustomerBo customerBO = (CustomerBo) BoFactory.getBoFactory().getBO(BoFactory.BOTypes.CUSTOMER);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = (ServletContext) getServletContext();
+        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
+        try (Connection connection = pool.getConnection()){
+
+            PrintWriter writer = resp.getWriter();
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+
+            ArrayList<CustomerDTO> all = customerBO.getAllCustomers(connection);
+
+            for (CustomerDTO customerDTO:all){
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+
+                customer.add("id",customerDTO.getCusId());
+                customer.add("name",customerDTO.getCusName());
+                customer.add("address",customerDTO.getCusAddress());
+                customer.add("contact",customerDTO.getCusSalary());
+
+                allCustomers.add(customer.build());
+            }
+            writer.print(allCustomers.build());
+
+
+        } catch (ClassNotFoundException e) {
+            resp.getWriter().println(e.getMessage());
+        } catch (SQLException e) {
+            resp.getWriter().println(e.getMessage());
+        }
+    }
 }
