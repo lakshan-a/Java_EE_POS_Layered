ab6249e8cf153f14a5dd1fe282dddcca6d0f60f9
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 18:51:12 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/dao/custom/impl/ItemDaoImpl.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/dao/custom/impl/ItemDaoImpl.java
@@ -1,16 +1,24 @@
 package lk.ijse.gdse.hello.dao.custom.impl;
 
+import lk.ijse.gdse.hello.dao.SQLUtil;
 import lk.ijse.gdse.hello.dao.custom.ItemDao;
 import lk.ijse.gdse.hello.entity.Item;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemDaoImpl implements ItemDao {
     @Override
     public ArrayList<Item> getAll(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<Item> allItems = new ArrayList<>();
+        ResultSet rst = SQLUtil.execute(connection,"SELECT * FROM item");
+        while (rst.next()) {
+            Item item = new Item(rst.getString(1), rst.getString(2), rst.getDouble(3),  rst.getInt(4));
+            allItems.add(item);
+        }
+        return allItems;
     }
 
     @Override
