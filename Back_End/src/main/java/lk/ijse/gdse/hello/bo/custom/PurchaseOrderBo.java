56b79d4ad9f3226c6627d3e7d81f57a5c26b0590
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 22:50:17 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/PurchaseOrderBo.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/PurchaseOrderBo.java
@@ -1,6 +1,22 @@
 package lk.ijse.gdse.hello.bo.custom;
 
 import lk.ijse.gdse.hello.bo.SuperBo;
+import lk.ijse.gdse.hello.dto.CustomerDTO;
+import lk.ijse.gdse.hello.dto.ItemDTO;
+import lk.ijse.gdse.hello.dto.OrderDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface PurchaseOrderBo extends SuperBo {
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException;
+
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException;
+
+    public boolean purchaseOrder(Connection connection, OrderDTO dto)throws SQLException, ClassNotFoundException;
+
+    public ArrayList<OrderDTO> getAllOrders(Connection connection) throws SQLException, ClassNotFoundException;
+
 }
