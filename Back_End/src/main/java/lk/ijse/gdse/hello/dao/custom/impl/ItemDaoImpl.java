a3b728288ea9870e1ce8753091276995bbe2513d
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 18:50:24 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/dao/custom/impl/ItemDaoImpl.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/dao/custom/impl/ItemDaoImpl.java
@@ -1,4 +1,30 @@
 package lk.ijse.gdse.hello.dao.custom.impl;
 
-public class ItemDaoImpl {
+import lk.ijse.gdse.hello.dao.custom.ItemDao;
+import lk.ijse.gdse.hello.entity.Item;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemDaoImpl implements ItemDao {
+    @Override
+    public ArrayList<Item> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(Connection connection, Item entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Connection connection, Item entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Connection connection, String Id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
 }
