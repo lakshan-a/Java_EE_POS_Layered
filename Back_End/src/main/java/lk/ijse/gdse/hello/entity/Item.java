0265e84e2c63e393c57ce3f939b20f62502c1e9b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 18:44:46 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/entity/Item.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/entity/Item.java
@@ -1,4 +1,58 @@
 package lk.ijse.gdse.hello.entity;
 
 public class Item {
+
+    private String ItemCode;
+    private String ItemName;
+    private double ItemPrice;
+    private int ItemQty;
+
+    public Item(String itemCode, String itemName, double itemPrice, int itemQty) {
+        ItemCode = itemCode;
+        ItemName = itemName;
+        ItemPrice = itemPrice;
+        ItemQty = itemQty;
+    }
+
+    public String getItemCode() {
+        return ItemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        ItemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public void setItemName(String itemName) {
+        ItemName = itemName;
+    }
+
+    public double getItemPrice() {
+        return ItemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        ItemPrice = itemPrice;
+    }
+
+    public int getItemQty() {
+        return ItemQty;
+    }
+
+    public void setItemQty(int itemQty) {
+        ItemQty = itemQty;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "ItemCode='" + ItemCode + '\'' +
+                ", ItemName='" + ItemName + '\'' +
+                ", ItemPrice=" + ItemPrice +
+                ", ItemQty=" + ItemQty +
+                '}';
+    }
 }
