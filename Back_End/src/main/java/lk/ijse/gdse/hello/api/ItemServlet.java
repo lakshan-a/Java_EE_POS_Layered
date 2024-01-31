f068946ab8ed7d2d54cfcd87fcfc659715f87161
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 19:11:31 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/api/ItemServlet.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/api/ItemServlet.java
@@ -1,11 +1,66 @@
 package lk.ijse.gdse.hello.api;
 
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.gdse.hello.bo.BoFactory;
 import lk.ijse.gdse.hello.bo.custom.ItemBo;
+import lk.ijse.gdse.hello.dto.ItemDTO;
+import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet(urlPatterns = "/items")
 public class ItemServlet extends HttpServlet {
 
     ItemBo itemBO = (ItemBo) BoFactory.getBoFactory().getBO(BoFactory.BOTypes.ITEM);
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        resp.setContentType("application/json");
+
+        ServletContext servletContext = getServletContext();
+        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
+
+        try(Connection connection = pool.getConnection()) {
+
+            PrintWriter writer = resp.getWriter();
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+
+            ArrayList<ItemDTO> all = itemBO.getAllItems(connection);
+
+            for (ItemDTO itemDTO:all) {
+
+                JsonObjectBuilder item = Json.createObjectBuilder();
+
+                item.add("code",itemDTO.getItemCode());
+                item.add("desc",itemDTO.getItemName());
+                item.add("unitPrice",itemDTO.getItemPrice());
+                item.add("qty",itemDTO.getItemQty());
+
+                allItems.add(item.build());
+            }
+
+            writer.print(allItems.build());
+
+
+        } catch (ClassNotFoundException e) {
+            resp.getWriter().println(e.getMessage());
+        } catch (SQLException e) {
+            resp.getWriter().println(e.getMessage());
+        }
+    }
 }
