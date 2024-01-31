dc78e3cfee168b128a581bd57e142ea7affa2857
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 21:58:05 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/api/ItemServlet.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/api/ItemServlet.java
@@ -1,8 +1,6 @@
 package lk.ijse.gdse.hello.api;
 
-import jakarta.json.Json;
-import jakarta.json.JsonArrayBuilder;
-import jakarta.json.JsonObjectBuilder;
+import jakarta.json.*;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -105,7 +103,42 @@ public class ItemServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+
+        String code = jsonObject.getString("code");
+        String name = jsonObject.getString("name");
+        Double price = Double.valueOf(jsonObject.getString("price"));
+        int qty = Integer.parseInt(jsonObject.getString("qty"));
+
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT,GET");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        ServletContext servletContext = getServletContext();
+        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
+
+
+        System.out.printf("code=%s ,name=%s ,price=%s ,qty=%s\n" , code,name,price,qty);
+
+        try(Connection connection = pool.getConnection()) {
+            PreparedStatement stn = connection.prepareStatement("UPDATE item SET description=?,unitPrice=?,qtyOnHand=? WHERE code=?");
+
+
+            stn.setString(1,name);
+            stn.setDouble(2,price);
+            stn.setInt(3,qty);
+            stn.setString(4,code);
+
+            stn.executeUpdate();
+            resp.getWriter().write("print!!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
