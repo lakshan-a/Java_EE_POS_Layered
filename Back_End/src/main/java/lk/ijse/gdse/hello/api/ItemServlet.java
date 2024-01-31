540a6da62947ad3ecca391312fe1f5f15e978425
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 21:44:05 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/api/ItemServlet.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/api/ItemServlet.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.gdse.hello.bo.BoFactory;
 import lk.ijse.gdse.hello.bo.custom.ItemBo;
+import lk.ijse.gdse.hello.dto.CustomerDTO;
 import lk.ijse.gdse.hello.dto.ItemDTO;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -28,32 +29,37 @@ public class ItemServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT,GET");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
 
-        resp.addHeader("Access-Control-Allow-Origin","*");
-        resp.setContentType("application/json");
+//        resp.addHeader("Access-Control-Allow-Origin","*");
+//        resp.setContentType("application/json");
+
 
         ServletContext servletContext = getServletContext();
         BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
 
-        try(Connection connection = pool.getConnection()) {
 
+        try (Connection connection = pool.getConnection()){
             PrintWriter writer = resp.getWriter();
 
             JsonArrayBuilder allItems = Json.createArrayBuilder();
 
             ArrayList<ItemDTO> all = itemBO.getAllItems(connection);
 
-            for (ItemDTO itemDTO:all) {
+            for (ItemDTO itemDTO:all){
+                JsonObjectBuilder customer = Json.createObjectBuilder();
 
-                JsonObjectBuilder item = Json.createObjectBuilder();
+                customer.add("code",itemDTO.getItemCode());
+                customer.add("name",itemDTO.getItemName());
+                customer.add("qty",itemDTO.getItemQty());
+                customer.add("price",itemDTO.getItemPrice());
 
-                item.add("code",itemDTO.getItemCode());
-                item.add("desc",itemDTO.getItemName());
-                item.add("unitPrice",itemDTO.getItemPrice());
-                item.add("qty",itemDTO.getItemQty());
-
-                allItems.add(item.build());
+                allItems.add(customer.build());
             }
+//            Json json = JsonBuilder.create();
+//            jsonb.toJson(customerList,resp.getWriter());
 
             writer.print(allItems.build());
 
@@ -62,102 +68,6 @@ public class ItemServlet extends HttpServlet {
             resp.getWriter().println(e.getMessage());
         } catch (SQLException e) {
             resp.getWriter().println(e.getMessage());
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        resp.addHeader("Access-Control-Allow-Origin", "*");
-        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT,GET");
-        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
-
-        jakarta.servlet.ServletContext servletContext = getServletContext();
-        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
-
-
-        String code = req.getParameter("code");
-        String description = req.getParameter("description");
-        double unitPrice = Double.parseDouble(req.getParameter("unitPrice"));
-        int qty = Integer.parseInt(req.getParameter("qtyOnHand"));
-
-        System.out.printf("code=%s ,description=%s ,unitPrice=%s ,qtyOnHand=%s\n" , code,description,unitPrice,qty);
-
-
-        try (Connection connection = pool.getConnection()){
-            PreparedStatement stn = connection.prepareStatement("INSERT INTO item(code,description,unitPrice,qtyOnHand) VALUES(?,?,?,?)");
-
-            stn.setString(1,code);
-            stn.setString(2,description);
-            stn.setDouble(3,unitPrice);
-            stn.setInt(4,qty);
-
-            stn.executeUpdate();
-            resp.getWriter().write("print!!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        resp.addHeader("Access-Control-Allow-Origin", "*");
-        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT,GET");
-        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
-
-        ServletContext servletContext = getServletContext();
-        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
-
-        String code = req.getParameter("code");
-        String description = req.getParameter("description");
-        double unitPrice = Double.parseDouble(req.getParameter("unitPrice"));
-        int qty = Integer.parseInt(req.getParameter("qtyOnHand"));
-
-        System.out.printf("code=%s ,description=%s ,unitPrice=%s ,qtyOnHand=%s\n" , code,description,unitPrice,qty);
-
-        try (Connection connection = pool.getConnection()){
-            PreparedStatement stn = connection.prepareStatement("UPDATE item SET description=?,unitPrice=?,qtyOnHand=? WHERE code=?");
-
-            stn.setString(1,code);
-            stn.setString(2,description);
-            stn.setDouble(3,unitPrice);
-            stn.setInt(4,qty);
-
-            stn.executeUpdate();
-            resp.getWriter().write("print!!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-        resp.addHeader("Access-Control-Allow-Origin", "*");
-        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT,GET");
-        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
-
-
-        ServletContext servletContext = getServletContext();
-        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
-
-        String code = req.getParameter("code");
-
-        try (Connection connection =pool.getConnection()){
-            PreparedStatement stm = connection.prepareStatement("DELETE FROM item WHERE code=?");
-            stm.setString(1,code);
-
-            if(stm.executeUpdate() != 0){
-                resp.setStatus(javax.servlet.http.HttpServletResponse.SC_NO_CONTENT);
-            }else{
-                resp.sendError(javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to delete the customer!");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
     }
 
