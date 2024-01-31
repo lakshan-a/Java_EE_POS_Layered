1fb4565030674fadd33c3a0a4e90f7f5aec6b915
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 18:53:59 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/ItemBo.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/bo/custom/ItemBo.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse.hello.bo.custom;
 
-public interface ItemBo {
+import lk.ijse.gdse.hello.bo.SuperBo;
+import lk.ijse.gdse.hello.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface ItemBo extends SuperBo {
+    public ArrayList<ItemDTO> getAllItems(Connection connection) throws SQLException, ClassNotFoundException;
+
+    public boolean saveItem(Connection connection ,ItemDTO itemDTO) throws SQLException, ClassNotFoundException;
+
+    public boolean updateItem(Connection connection,ItemDTO itemDTO) throws SQLException, ClassNotFoundException;
+    public boolean deleteItem(Connection connection,String itemCode) throws SQLException, ClassNotFoundException;
+
 }
