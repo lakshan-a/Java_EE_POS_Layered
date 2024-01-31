9f6b34d55a1299c5e37ab578e653eb17f3dc3d2c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 22:29:33 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/entity/Orders.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/entity/Orders.java
@@ -1,4 +1,47 @@
 package lk.ijse.gdse.hello.entity;
 
 public class Orders {
+
+    private String orderID;
+    private String date;
+    private String customerID;
+
+    public Orders(String orderID, String date, String customerID) {
+        this.orderID = orderID;
+        this.date = date;
+        this.customerID = customerID;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "orderID='" + orderID + '\'' +
+                ", date='" + date + '\'' +
+                ", customerID='" + customerID + '\'' +
+                '}';
+    }
 }
