9ac34202ef7f1556abba962821d65f9a826b19f2
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-31 23:08:04 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/api/PurchaseOrderServlet.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/api/PurchaseOrderServlet.java
@@ -1,8 +1,23 @@
 package lk.ijse.gdse.hello.api;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse.hello.bo.BoFactory;
+import lk.ijse.gdse.hello.bo.custom.PurchaseOrderBo;
+
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/placeOrder")
 public class PurchaseOrderServlet extends HttpServlet {
+
+    PurchaseOrderBo purchaseOrderBO= (PurchaseOrderBo) BoFactory.getBoFactory().getBO(BoFactory.BOTypes.PURCHASE_ORDER);
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
