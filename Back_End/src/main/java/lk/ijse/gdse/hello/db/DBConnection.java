5274cad55481bbf02896321080bc229618725b46
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-04 22:51:01 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/db/DBConnection.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/db/DBConnection.java
@@ -6,5 +6,21 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static DBConnection dbConnection;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/thogakade";
+        String unicode = "?useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection(url + unicode, "root", "12345");
+    }
+
+    public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
+        return dbConnection == null ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 
 }
