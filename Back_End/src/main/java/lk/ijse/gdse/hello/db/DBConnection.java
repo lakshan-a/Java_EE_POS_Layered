44ed1c1db6260db111636edee113794800615147
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-04 22:50:39 +0530

    updated

---
+++ Back_End/src/main/java/lk/ijse/gdse/hello/db/DBConnection.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.hello.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+
+}
