90986d1f72e4ca2597c36df84c61f0d05c8a4b57
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-05 21:55:28 +0530

    updated

--- Back_End/src/main/java/lk/ijse/gdse/hello/api/PurchaseOrderServlet.java
+++ Back_End/src/main/java/lk/ijse/gdse/hello/api/PurchaseOrderServlet.java
@@ -20,6 +20,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/orders")
@@ -66,7 +67,75 @@ public class PurchaseOrderServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        ServletContext servletContext
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT,GET");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+
+        ArrayList<OrderDetailDTO> orderDetailsDTOS = new ArrayList<>();
+
+
+        ServletContext servletContext =getServletContext();
+        BasicDataSource pool = (BasicDataSource) servletContext.getAttribute("dbcp");
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+
+        String oID = jsonObject.getString("orderId");
+        String oDate = jsonObject.getString("date");
+        String oCusID = jsonObject.getString("cusId");
+        JsonArray oCartItems = jsonObject.getJsonArray("itemDet");
+
+
+        System.out.println(oCusID);
+        System.out.println(oDate);
+        System.out.println(oID);
+
+        try {
+            Connection connection = pool.getConnection();
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO orders VALUES (?,?,?)");
+
+            preparedStatement.setString(1,oID);
+            preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
+            preparedStatement.setString(3,oCusID);
+
+            int i = preparedStatement.executeUpdate();
+            connection.commit();
+
+            if (i>0){
+
+                Connection connection1 = pool.getConnection();
+                connection1.setAutoCommit(false);
+
+                PreparedStatement preparedStatement1 = connection1.prepareStatement("INSERT INTO orderdetail VALUES (?,?,?,?)");
+
+                for (JsonValue obj : oCartItems){
+
+                    JsonObject jsonObject1 = obj.asJsonObject();
+                    String code = jsonObject1.getString("code");
+                    Integer avQty = Integer.valueOf(jsonObject1.getString("avQty"));
+                    Double price = Double.valueOf(jsonObject1.getString("price"));
+
+                    preparedStatement1.setString(1,oID);
+                    preparedStatement1.setString(2,code );
+                    preparedStatement1.setInt(3,avQty);
+                    preparedStatement1.setDouble(4,price);
+
+                    preparedStatement1.executeUpdate();
+
+
+                }
+                connection1.commit();
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+
+        }
+
     }
 
     @Override
