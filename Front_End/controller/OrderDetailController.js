65ac4ba707fd029b0d520b031fe815c7833124bd
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-06 11:45:54 +0530

    updated

--- Front_End/controller/OrderDetailController.js
+++ Front_End/controller/OrderDetailController.js
@@ -1,14 +1,24 @@
-var orderDetails = [];
+loadOrderDetail();
 
-loadAllOrderDetails();
+function loadOrderDetail(){
 
-function loadAllOrderDetails() {
+    $.ajax({
+        // url : "http://localhost:8080/app/orders",
+        success : function(res){
+            let orderDetail = $(res);
+            $('#orderDetailTable').empty();
 
-    $("#orderDetailTable").empty();
+            for(let i = 0; i < orderDetail.length; i++){
+                let orderId = orderDetail[i].orderId;
+                let id = orderDetail[i].id;
+                let total = orderDetail[i].total;
+                let qty = orderDetail[i].qty;
+                let date = orderDetail[i].date;
 
-    for (var orderDetail of orderDetails) {
-        console.log(orderDetail);
-        var row = `<tr><td>${orderDetail.orderId}</td><td>${orderDetail.cusId}</td><td>${orderDetail.total}</td><td>${orderDetail.qty}</td><td>${orderDetail.date}</td></tr>`;
-        $("#tblOrderDetails").append(row);
-    }
+                let row =`<tr><td>${orderId}</td><td>${id}</td><td>${total}</td><td>${qty}</td><td>${date}</td></tr>`;
+                $('#orderDetailTable').append(row);
+            }
+        }
+    });
+
 }
